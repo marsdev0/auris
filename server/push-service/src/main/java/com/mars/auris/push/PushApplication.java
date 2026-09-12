0ad1c47bbf1ce785f08ccd09a764c4a47e857916
marsdev0 <2277087813@qq.com>
2026-09-13 02:12:50 +0800

    fix(push): set jvm default timezone to utc

--- server/push-service/src/main/java/com/mars/auris/push/PushApplication.java
+++ server/push-service/src/main/java/com/mars/auris/push/PushApplication.java
@@ -3,12 +3,18 @@ package com.mars.auris.push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 /**
  * @author geyan
  * @date 2026/8/17
  */
 @SpringBootApplication
 public class PushApplication {
+
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PushApplication.class, args);
